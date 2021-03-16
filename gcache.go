400069f6e8b7ad54217cf53ffac11c7a7755f979
mamil <mamilgithub@gmail.com>
2021-03-16 22:14:25 +0800

    adding gcache

---
+++ gcache.go
@@ -0,0 +1,45 @@
+package gCache
+
+import "sync"
+
+type Group struct{
+	name string
+	mainCache cache
+}
+
+var (
+	mu sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string, cacheBytes int64) *Group{
+	mu.Lock()
+	defer mu.Unlock()
+	g:=&Group{
+		name: name,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group{
+	mu.RLock()
+	g:=groups[name]
+	mu.RUnlock()
+	return g
+}
+
+
+func (g *Group) Get(key string) (ByteView, error){
+	if key == ""{
+		return ByteView{}, fmt.Errorf("key is empty")
+	}
+
+	if v, ok := g.mainCache.get(key); ok{
+		log.Println("gcache hit")
+		return v, nil
+	}
+
+	// TODO load
+}
