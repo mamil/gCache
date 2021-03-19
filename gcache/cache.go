177fbe5d7cca06c288bd02d6e1eb39596d4a9a7d
mamil <mamilgithub@gmail.com>
2021-03-19 10:43:33 +0200

    add comments

--- gcache/cache.go
+++ gcache/cache.go
@@ -11,6 +11,7 @@ type cache struct {
 	cacheBytes int64
 }
 
+// 向lru添加数据
 func (c *cache) add(key string, value ByteView) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -20,6 +21,7 @@ func (c *cache) add(key string, value ByteView) {
 	c.lru.Add(key, value)
 }
 
+// 从lru读取数据
 func (c *cache) get(key string) (value ByteView, ok bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
