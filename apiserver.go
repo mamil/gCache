d27b7cb5228b0a59204944e1664ea9f1e8e905e5
raymonder jin <rayjingithub@gmail.com>
2021-12-20 19:41:46 +0800

    separate apiserver

---
+++ apiserver.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"gCache/gcache"
+	"log"
+	"net/http"
+)
+
+// 启动API服务
+func startAPIServer(apiAddr string, g *gcache.Group) {
+	http.Handle("/api", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			key := r.URL.Query().Get("key")
+			view, err := g.Get(key)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/octet-stream")
+			w.Write(view.ByteSlice())
+
+		}))
+	log.Println("fontend server is running at", apiAddr)
+	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+}
--- main.go
+++ main.go
@@ -44,24 +44,6 @@ func startCacheServer(addr string, addrs []string, g *gcache.Group) {
 	log.Fatal(http.ListenAndServe(addr[7:], peers))
 }
 
-// 启动API服务
-func startAPIServer(apiAddr string, g *gcache.Group) {
-	http.Handle("/api", http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			key := r.URL.Query().Get("key")
-			view, err := g.Get(key)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/octet-stream")
-			w.Write(view.ByteSlice())
-
-		}))
-	log.Println("fontend server is running at", apiAddr)
-	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
-}
-
 func initConfig() error {
 	viper.SetConfigFile("./config.ini")
 	if err := viper.ReadInConfig(); err != nil {
